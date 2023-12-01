4c147d54c8b7d052af53b026d6c3994f200ec078
Prafful Singhal <praffuls@Praffuls-MacBook-Pro.local>
2023-12-01 17:13:19 +0530

    button changes

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -5,5 +5,18 @@
  */
 
 export default async function decorate(block) {
-  console.log(block);
+console.log(block);
+const anchorElement = document.createElement('a');
+const divElement = document.createElement('div');
+const paraElement = document.createElement('p');
+anchorElement.href= block.lastElementChild.firstElementChild.innerHTML;
+anchorElement.title = block.firstElementChild.firstElementChild.innerText;
+anchorElement.html = block.firstElementChild.firstElementChild.innerText;
+
+
 }
+
+
+
+
+<div class="default-content-wrapper"><p class="button-container"><a href="/HOLs/CSC/Global/the-content-supply-chain" title="Let's understand Content Supply Chain" class="button">Let's understand Content Supply Chain</a></p></div>
