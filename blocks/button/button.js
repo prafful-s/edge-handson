1ab8aa59e05f3513542d87b67d202c0ea8571efe
Prafful Singhal <praffuls@Praffuls-MacBook-Pro.local>
2023-12-01 17:15:42 +0530

    button changes

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -15,8 +15,3 @@ anchorElement.html = block.firstElementChild.firstElementChild.innerText;
 
 
 }
-
-
-
-
-<div class="default-content-wrapper"><p class="button-container"><a href="/HOLs/CSC/Global/the-content-supply-chain" title="Let's understand Content Supply Chain" class="button">Let's understand Content Supply Chain</a></p></div>
