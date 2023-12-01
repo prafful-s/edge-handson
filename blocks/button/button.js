f5ae6bb0c83d24f9b1651965b50bf51bd9d1de5f
Prafful Singhal <praffuls@Praffuls-MacBook-Pro.local>
2023-12-01 17:19:00 +0530

    button changes

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -7,11 +7,11 @@
 export default async function decorate(block) {
 console.log(block);
 const anchorElement = document.createElement('a');
-const divElement = document.createElement('div');
 const paraElement = document.createElement('p');
 anchorElement.href= block.lastElementChild.firstElementChild.innerHTML;
 anchorElement.title = block.firstElementChild.firstElementChild.innerText;
 anchorElement.html = block.firstElementChild.firstElementChild.innerText;
-
-
+paraElement.class=button-container;
+paraElement.append(anchorElement);
+block.append(block);
 }
